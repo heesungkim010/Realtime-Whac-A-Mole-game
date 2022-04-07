c0a0f4000d94687dce08bf4b7c47a6f5429fb004
heesungkim010 <heesungkim010@gmail.com>
2022-04-07 17:06:46 +0900

    rename methods

--- src/main/java/com/example/whac_a_mole/api/LoginController.java
+++ src/main/java/com/example/whac_a_mole/api/LoginController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -15,11 +18,16 @@ public class LoginController {
     private final MemberRepository memberRepository;
 
     @PostMapping("/api/members/login")
-    public LoginMemberResponse loginMember(@RequestBody CreateMemberRequest request){ // request received
+    public LoginMemberResponse loginMember(@RequestBody CreateMemberRequest request,
+                                           HttpServletRequest servletRequest){ // request received
         log.info("userId : {}",request.userId);
         boolean isLoginSuccess = memberRepository.saveMember(request.userId);
 
         if (isLoginSuccess){ // login success.
+            //if login success, set session.
+            HttpSession session = servletRequest.getSession(true);
+            session.setAttribute(SessionConst.LOGIN_MEMBER, request.userId);
+            //save session-data
             return new LoginMemberResponse(true); //did_login : true
         }
 
---
+++ src/main/java/com/example/whac_a_mole/api/SessionConst.java
@@ -0,0 +1,5 @@
+package com.example.whac_a_mole.api;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
